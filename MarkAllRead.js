18ff1c555cff74bd72e4b1729a1c51e9576dab41
rcovery <ryanczpereira@gmail.com>
2021-08-15 18:10:34 -0300

    feat: read all email [protonmail]

---
+++ MarkAllRead.js
@@ -0,0 +1,19 @@
+var index = 0
+
+setInterval(() => {
+	document.querySelector("#\\:4 > div").click()
+
+	setTimeout(() => {
+		document.querySelector("#\\:ok").click()
+	}, 500)
+
+	setTimeout(() => {
+		document.querySelector("#\\:4 > div > div.nH.aqK > div.Cq.aqL > div > div > div:nth-child(3) > div.T-I.J-J5-Ji.m9.T-I-ax7.L3").click()
+	}, 1000)
+
+	setTimeout(() => {
+		document.querySelector("#\\:175").click()
+	}, 2000)
+
+	index++
+}, 5500)
